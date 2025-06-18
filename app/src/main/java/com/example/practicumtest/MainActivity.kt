0879bb09795518aef4d38dc04abb05adf0ceb157
Trevor10 <trevormoodley10@gmail.com>
2025-06-18 12:21:07 +0200

    Changed name

--- app/src/main/java/com/example/practicumtest/MainActivity.kt
+++ app/src/main/java/com/example/practicumtest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PracticumTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
